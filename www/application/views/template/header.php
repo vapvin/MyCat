5eeb0f173845f2750f67d4f21a7b4d2e1183753b
eaim <eeiim@naver.com>
2019-11-13 15:56:55 +0900

    업로드

--- www/application/views/template/header.php
+++ www/application/views/template/header.php
@@ -5,5 +5,6 @@
 	<title>소반 (소중반 반려동물)</title>
 
 	<link rel="stylesheet" type="text/css" href="<?=_CSS_DIR_?>/reset.css">
+	<link rel="stylesheet" type="text/css" href="<?=_CSS_DIR_?>/main.css">
 </head>
 <body>
